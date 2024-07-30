a11e1f4a94c84ff67c1098703330dee2411412fe
Walisson <154024369+Sileniz@users.noreply.github.com>
2024-07-30 07:06:19 +0000

    fix scrolltoop

--- src/components/ScrollTop.jsx
+++ src/components/ScrollTop.jsx
@@ -1,8 +1,12 @@
 import styles from "../styles/ScrollTop.module.css"
 import { FaArrowUp } from "react-icons/fa";
 export default function ScrollTop({disabled}){
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      };
+    
     return(
-        <div className={disabled ? styles.myBtn : styles.myBtnNah}>
+        <div className={disabled ? styles.myBtn : styles.myBtnNah} onClick={scrollToTop}>
             <FaArrowUp className={styles.ArrowUp}/>
         </div>
     )
